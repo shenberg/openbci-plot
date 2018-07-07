11a04c6a76b1d520621f6bc5b5210b1bd3fae5be
Orian Sharoni <orian@orian2-mac.local>
2018-07-07 15:07:00 +0300

    css

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,17 @@ module.exports = {
       title: 'Output Management'
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
